d448b7b6a7fa61d1896a07a677c2ec55661851cf
khoahre123 <e0550384@u.nus.edu>
2022-04-03 18:05:47 +0800

    Fix full outer join

--- Part_2/khoa.sql
+++ Part_2/khoa.sql
@@ -153,10 +153,10 @@ DECLARE
         ),
         product_rate AS (
             SELECT R1.id, ROUND(R1.num_accept/R2.total_refund, 2) as rate
-            FROM product_accept_rate R1 NATURAL JOIN product_total R2
+            FROM product_accept_rate R1 FULL OUTER JOIN product_total R2 ON R1.id = R2.id
         )
         SELECT P.id, P.name, coalesce(Re.rate, 0)
-        FROM product P JOIN product_rate R ON P.id = R.id
+        FROM product P FULL OUTER JOIN product_rate R ON P.id = R.id
         WHERE P.manufacturer = manufacturer_id
         ORDER BY rate DESC, P.id
     );
