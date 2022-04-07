c8c7eff2287ad37e8baf1d70eac9873f2364d256
pbthang <bathang02@gmail.com>
2022-04-08 00:18:23 +0800

    fix bug

--- Part_2/procedures.sql
+++ Part_2/procedures.sql
@@ -1,44 +1,45 @@
 -- (1) place_order
 
 CREATE OR REPLACE PROCEDURE place_order( 
-    user_id INTEGER, coupon_id INTEGER, shipping_address TEXT, 
+    user_id INTEGER, _coupon_id INTEGER, shipping_address TEXT, 
     shop_ids INTEGER[], product_ids INTEGER[], sell_timestamps TIMESTAMP[], 
     quantities INTEGER[], shipping_costs NUMERIC[]
 ) AS $$
 DECLARE
-    payment_amount NUMERIC;
+    payment NUMERIC;
     order_id INTEGER;
     i INTEGER;
     price NUMERIC;
 BEGIN
-    INSERT INTO Orders AS O
-    VALUES (DEFAULT, user_id, NULL, shipping_address, 0)
-    RETURNING O.id INTO order_id;
-    payment_amount := 0;
+    order_id := (SELECT MAX(id) FROM Orders) + 1;
+    INSERT INTO Orders(id, user_id, coupon_id, shipping_address, payment_amount)
+    VALUES (order_id, user_id, NULL, shipping_address, 0);
+    payment := 0;
 
-    FOR i IN 1..array_upper(product_ids)
+    FOR i IN 1..array_upper(product_ids, 1)
     LOOP
         INSERT INTO Orderline
         VALUES (order_id, shop_ids[i], product_ids[i], sell_timestamps[i], quantities[i], shipping_costs[i], 'being_processed', NULL);
         
-        UPDATE Sells AS S
-        SET S.quantity = S.quantity - quantities[i]
-        WHERE ROW(S.shop_id, S.product_id, S.sell_timestamp) = ROW(shop_ids[i], product_ids[i], sell_timestamps[i]);
+        UPDATE Sells
+        SET quantity = quantity - quantities[i]
+        WHERE ROW(shop_id, product_id, sell_timestamp) = ROW(shop_ids[i], product_ids[i], sell_timestamps[i]);
         
         price := (
             SELECT S.price
             FROM Sells S
             WHERE ROW(S.shop_id, S.product_id, S.sell_timestamp) = ROW(shop_ids[i], product_ids[i], sell_timestamps[i])
         );
-        payment_amount := payment_amount + quantities[i] * price + shipping_costs[i];
+        payment := payment + quantities[i] * price + shipping_costs[i];
     END LOOP;
 
-    UPDATE Orders AS O
-    SET O.payment_amount = payment_amount - (
+    RAISE NOTICE 'payment: %', payment;
+    UPDATE Orders
+    SET payment_amount = payment - COALESCE((
         SELECT C.reward_amount
         FROM Coupon_batch C
-        WHERE C.id = coupon_id
-    ), O.coupon_id = coupon_id
-    WHERE O.id = order_id;
+        WHERE C.id = _coupon_id
+    ), 0), coupon_id = _coupon_id
+    WHERE id = order_id;
 END;
 $$ LANGUAGE plpgsql;
